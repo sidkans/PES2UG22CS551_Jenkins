cfe361dbfc465d0a9d68f7c6b7bd4b637623ab41
sidkans <sid_kans@hotmail.com>
2025-03-11 16:13:28 +0530

    PES2UG22CS551:TASK4 CHANGE

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,12 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "PES2UG22CS551: HAS MADE A CHANGE TO THIS CODE NOW";
   return 0;
 }
