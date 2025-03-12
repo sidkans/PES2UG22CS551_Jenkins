1b1c0e1f0159528c742e7979bf0bcf6ae2b0fa55
Siddharth K <76964658+sidkans@users.noreply.github.com>
2025-03-12 23:11:19 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void)
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "PES2UG22CS551 - Siddharth K";
+  cout << "PES2UG22CS551 - Siddharth K - (12-03-2025)";
   return 0;
 }
